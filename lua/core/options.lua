34862f65d5e3c98facdfe5f7d853b756632e59d4
Sebastien <moi@sebastientalbot.com>
2022-01-21 16:59:39 -0500

    Init core/options with basics

---
+++ lua/core/options.lua
@@ -0,0 +1,20 @@
+local opt = vim.opt
+
+opt.clipboard = "unnamedplus"
+opt.cursorline = true
+opt.expandtab = true
+opt.ignorecase = true
+opt.lazyredraw = true
+opt.linebreak = true
+opt.number = true
+opt.relativenumber = true
+opt.shiftwidth = 2
+opt.showmatch = true
+opt.showmatch = true
+opt.smartcase = true
+opt.swapfile = false
+opt.tabstop = 2
+opt.undodir = "./undo"
+opt.undofile = true
+
+-- vim.cmd [[colorscheme tokyonight]]
