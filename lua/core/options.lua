e8acb5915c33ee6292a9942c8d6724c9ddbae793
Sebastien Talbot <sebastien@chefcookit.com>
2022-01-26 22:01:38 -0500

    Use old options and clean defaults

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,19 +1,21 @@
 local opt = vim.opt
 
 opt.clipboard = "unnamedplus"
+opt.colorcolumn = "80"
 opt.cursorline = true
 opt.expandtab = true
 opt.ignorecase = true
 opt.lazyredraw = true
 opt.linebreak = true
+opt.mouse = ""
 opt.number = true
 opt.relativenumber = true
 opt.shiftwidth = 2
-opt.showmatch = true
 opt.showmatch = true
 opt.smartcase = true
 opt.swapfile = false
 opt.tabstop = 2
 opt.termguicolors = true
-opt.undodir = "./undo"
+opt.timeout = false
+opt.undodir = "~/.config/nvim/undo"
 opt.undofile = true
