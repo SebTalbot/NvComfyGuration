88741c55a181645eef33cafb71fb03b37c413e84
Sebastien Talbot <sebastien@chefcookit.com>
2022-01-28 02:45:51 -0500

    Plugins.Nvim_cmp add lsp

--- lua/plugins/configs/nvim_cmp.lua
+++ lua/plugins/configs/nvim_cmp.lua
@@ -96,6 +96,7 @@ cmp.setup {
       vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
       -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       vim_item.menu = ({
+        nvim_lsp = "[LSP]",
         luasnip = "[Snippet]",
         buffer = "[Buffer]",
         path = "[Path]",
@@ -104,6 +105,7 @@ cmp.setup {
     end,
   },
   sources = {
+    { name = "nvim_lsp" },
     { name = "luasnip" },
     { name = "buffer" },
     { name = "path" },
