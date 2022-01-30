664dd3d50bc51f0123071722e47de055353ace9a
Sebastien Talbot <sebastien@chefcookit.com>
2022-01-30 18:25:22 -0500

    LSP add jsonls

--- lua/plugins/configs/lsp/lsp_install.lua
+++ lua/plugins/configs/lsp/lsp_install.lua
@@ -14,7 +14,7 @@ local servers = {
   -- "html",
   -- "pyright",
   -- "remark_ls",
-  -- "jsonls",
+  "jsonls",
   "sumneko_lua",
   "tsserver",
   -- "vimls",
@@ -23,7 +23,7 @@ local servers = {
 for _, lsp_name in pairs(servers) do
   local server_is_found, server = lsp_installer.get_server(lsp_name)
   if server_is_found and not server:is_installed() then
-    print("Installing " .. lsp_name .. "...")
+    print("[LSP] Installing " .. lsp_name .. "...")
     server:install()
   end
 end
@@ -39,11 +39,15 @@ lsp_installer.settings({
 })
 
 lsp_installer.on_server_ready(function(server)
-  print("starting" .. server.name)
 	local opts = {
 		on_attach = require("plugins.configs.lsp.handlers").on_attach,
 		capabilities = require("plugins.configs.lsp.handlers").capabilities(),
 	}
+
+	 if server.name == "jsonls" then
+	 	local settings = require("plugins.configs.lsp.settings.jsonls")
+	 	opts = vim.tbl_deep_extend("force", settings, opts)
+	 end
 
 	 if server.name == "sumneko_lua" then
 	 	local settings = require("plugins.configs.lsp.settings.sumneko_lua")
