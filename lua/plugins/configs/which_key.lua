1221f80696eabcd5c29b9f2ab8e163083ad14520
Sebastien Talbot <sebastien@chefcookit.com>
2022-02-08 01:52:42 -0500

    Which-key Current Mapping

--- lua/plugins/configs/which_key.lua
+++ lua/plugins/configs/which_key.lua
@@ -7,25 +7,93 @@ end
 local setup = {}
 
 local nmappings = {
+  -- Better definitions
+  ["<S-k>"] = "Hover definition",
+  ["["] = "Previous motions",
+  ["]"] = "Next motions",
+  z = {
+    name = "Misc",
+  },
+  g = {
+    name = "Misc",
+    c = "Comment",
+  },
+
+  -- Leader Menu
   ["<leader>"] = {
+    name = "Leader menu",
+    ["<leader>"] = "Default normal mapping",
+    ["*"] = "Select word under cursor",
+    ["/"] = "Hide word selection",
+    ["h"] = "Neovim help search",
+    a = {
+      name = "Actions",
+      r = "Rename word under the cursor @TO MOVE",
+    },
+    b = {
+      name = "Buffers",
+      b = "Back",
+      n = "Next",
+      p = "Previous",
+      d = {
+        name = "Close",
+        d = "Current",
+        o = "Everything but the current",
+      },
+    },
+    e = {
+      name = "Errors & Diagnostic",
+      e = "Open details",
+      l = "List diagnostic in quickfix",
+      n = "Next error",
+      p = "Previous error",
+    },
+    f = {
+      name = "Find",
+      a = "All Files",
+      b = "Buffers",
+      f = "Files (not hidden)",
+      m = "Format file @TO MOVE",
+      w = "Live Grep",
+    },
+    l = {
+      name = "LSP",
+      d = "List definitions",
+      i = "List implementations",
+      l = "Jump to declaration",
+      r = "List references",
+      t = "List type definitions",
+      a = {
+        name = "Actions",
+        a = "LSP code actions",
+      }
+    },
+    n = {
+      name = "Neotree",
+      n = "Open on current file",
+      t = "Toggle",
+    },
     w = {
       name = "Windows",
-      H = "Move Position to Right",
-      J = "Move Position to Down",
-      K = "Move Position to Up",
-      L = "Move Position to Left",
-      d = "Close Current",
-      h = "Select Right",
-      j = "Select Down",
-      k = "Select Up",
-      l = "Select Left",
-      s = "Split Horizontal",
-      v = "Split Vertical",
+      H = "Move position to right",
+      J = "Move position to down",
+      K = "Move position to up",
+      L = "Move position to left",
+      d = "Close current",
+      h = "Select right",
+      j = "Select down",
+      k = "Select up",
+      l = "Select left",
+      s = "Split horizontal",
+      v = "Split vertical",
       r = {
         name = "Resize",
         e = "Equal",
         r = "Smart",
       },
+    },
+    z = {
+      name = "System files",
     },
   },
 }
