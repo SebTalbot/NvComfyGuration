94acd346340b198777429d973490f674e8ad0cd6
Sebastien Talbot <sebastien@chefcookit.com>
2022-02-09 02:35:52 -0500

    Nvim tree customize

--- lua/plugins/configs/nvim_tree.lua
+++ lua/plugins/configs/nvim_tree.lua
@@ -1,42 +1,12 @@
 require("nvim-tree").setup {
-  disable_netrw = true,
-  hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = {},
-  auto_close = false,
-  open_on_tab = false,
-  hijack_cursor = false,
-  update_cwd = false,
-  update_to_buf_dir = {
-    enable = true,
-    auto_open = true,
-  },
-  diagnostics = {
-    enable = false,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    },
-  },
-  update_focused_file = {
-    enable = false,
-    update_cwd = false,
-    ignore_list = {},
-  },
-  system_open = {
-    cmd = nil,
-    args = {},
-  },
   filters = {
     dotfiles = false,
     custom = {},
   },
   git = {
-    enable = true,
-    ignore = true,
-    timeout = 500,
+    enable = false,
+    ignore = false,
+    timeout = 400,
   },
   view = {
     width = 30,
@@ -48,13 +18,14 @@ require("nvim-tree").setup {
       custom_only = false,
       list = {},
     },
-    number = false,
-    relativenumber = false,
-    signcolumn = "yes",
+    number = true,
+    relativenumber = true,
+    signcolumn = "auto",
   },
-  trash = {
-    cmd = "trash",
-    require_confirm = true,
+  actions = {
+    open_file = {
+      quit_on_open = true,
+    },
   },
 }
 
