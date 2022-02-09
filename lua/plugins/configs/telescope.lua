259c020adfdf9ade8f24cf832b11b45b305901b6
Sebastien Talbot <sebastien@chefcookit.com>
2022-02-09 02:36:35 -0500

    Telescope customize

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -5,12 +5,51 @@ if not present then
   return
 end
 
+local actions = require "telescope.actions"
+local actions_layout = require "telescope.actions.layout"
+
 telescope.setup {
+  defaults = {
+    multi_icon = "",
+    selection_caret = "",
+    prompt_prefix = "  ",
+    entry_prefix = "",
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--column",
+      "--smart-case",
+      "--trim",
+    },
+    mappings = {
+      i = {
+        ["<C-a>"] = actions.toggle_all,
+        ["<C-e>"] = actions_layout.toggle_preview,
+      },
+    },
+  },
   pickers = {
+    buffers = {
+      mappings = {
+        i = {
+          ["<C-b>"] = actions.delete_buffer,
+        },
+      },
+    },
+    find_files = {
+      follow = true,
+      path_display = { "smart" },
+    },
+    live_grep = {
+      disable_coordinates = true,
+      path_display = { "tail" },
+    },
     help_tags = {
       mappings = {
         i = {
-          ["<CR>"] = require("telescope.actions").select_vertical,
+          ["<CR>"] = actions.select_vertical,
         },
       },
     },
@@ -19,6 +58,14 @@ telescope.setup {
     },
     git_bcommits = {
       git_command = { "git", "log", "--pretty=format:%h %<(15,trunc)%an %ad %s", "--date=format:%Y-%m-%d %H:%M:%S" },
+    },
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true,
+      override_generic_sorter = true,
+      override_file_sorter = true,
+      case_mode = "smart_case",
     },
   },
 }
