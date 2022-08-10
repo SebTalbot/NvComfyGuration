fd9e12fcb04b37381b648b37fa4e69c61f1fa953
Sebastien Talbot <moi@sebastientalbot.com>
2022-08-09 23:31:10 -0400

    add DAP breakpoints signs

--- lua/plugins/configs/dap/init.lua
+++ lua/plugins/configs/dap/init.lua
@@ -1,1 +1,5 @@
 require("core.mappings").dap()
+
+vim.fn.sign_define('DapBreakpoint', {text=' ', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapBreakpointRejected', {text=' ', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapStopped', {text=' ', texthl='', linehl='', numhl=''})
