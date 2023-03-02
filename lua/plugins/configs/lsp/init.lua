a7088c7b21a5d8d842470d139e1c64d25259f2ce
Sebastien Talbot <moi@sebastientalbot.com>
2023-03-02 10:46:02 -0500

    Update sumneko to lua_ls

--- lua/plugins/configs/lsp/init.lua
+++ lua/plugins/configs/lsp/init.lua
@@ -25,7 +25,7 @@ lspconfig.gopls.setup(handlers.inject({}))
 lspconfig.graphql.setup(handlers.inject({}))
 lspconfig.html.setup(handlers.inject({}))
 lspconfig.jsonls.setup(handlers.inject(require("plugins.configs.lsp.settings.jsonls")))
-lspconfig.sumneko_lua.setup(handlers.inject(require("plugins.configs.lsp.settings.sumneko_lua")))
+lspconfig.lua_ls.setup(handlers.inject(require("plugins.configs.lsp.settings.lua_ls")))
 lspconfig.vimls.setup(handlers.inject({}))
 
 require("plugins.configs.lsp.settings.tsserver")
