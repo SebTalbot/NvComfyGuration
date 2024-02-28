46b5873befebe7cb641f73cc4613fc644a227ebb
Sebastien Talbot <moi@sebastientalbot.com>
2024-02-28 00:40:31 -0500

    pyenv python provider

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -32,3 +32,16 @@ end
 vim.cmd([[ let g:loaded_perl_provider = 0 ]])
 vim.cmd([[ set title titlestring=%{substitute(getcwd(),\ $HOME,\ '~',\ '')} ]])
 vim.cmd([[ set foldexpr=nvim_treesitter#foldexpr() ]])
+
+-- Set pyenv virtual env if exists
+local pyenv_root = io.popen([[pyenv root]])
+if pyenv_root then
+  local pyenv_root_path = pyenv_root:read("*a")
+  pyenv_root:close()
+  local venv_path = pyenv_root_path:gsub("\n", "") .. "/versions/py3nvim/bin/python"
+  local test_venv = io.open(venv_path, "r")
+  local venv_exists = test_venv ~= nil and io.close(test_venv)
+  if venv_exists then
+    vim.g.python3_host_prog = venv_path
+  end
+end
